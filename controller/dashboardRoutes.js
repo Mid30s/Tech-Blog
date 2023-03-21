275e5978601de16ffc52b85b39dd8f536da4e9ff
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-03-22 00:24:16 +1030

    updated homeroutes and dashborardRoutes files

---
+++ controller/dashboardRoutes.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+// Get all posts for dashboard
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      include: [{ model: User, attributes: ["name"] }],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("dashboard", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// edit post by id
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ["name"] }],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render("editPost", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
