3be6c22819caa87fc58ccc59cd4b0b177afaaf4c
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-03-22 02:02:05 +1030

    added eslint files and prettier files, added tailwind css framework and related tailwind css files

--- utils/auth.js
+++ utils/auth.js
@@ -2,9 +2,9 @@ const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the user to the login page
 
   if (!req.session.loggedIn) {
-    res.redirect('/login');
+    res.redirect("/login");
   } else {
-    // If the user is logged in, execute the route function 
+    // If the user is logged in, execute the route function
     // We call next() if the user is authenticated
     next();
   }
